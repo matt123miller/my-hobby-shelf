b8431b0f88fc3fb5378d2ff9e8ae8850d10c30a6
Matt Miller <matt123miller@hotmail.co.uk>
2020-08-25 20:11:24 +0100

    Don't want to use css filter, want to learn about colours. But trying it mnyself with rgb wrap around maths isn't working right

--- paint-mixer-frontend/src/components/ComplimentaryColours.jsx
+++ paint-mixer-frontend/src/components/ComplimentaryColours.jsx
@@ -7,10 +7,51 @@ function ComplimentaryColours(props){
 
     const { chosenColour } = props;
 
-    let complimentaryColour = '#cdbdf5';
+    // set to the opposite of our chosen colour
+    let colourAsRgb = hexToRgb(chosenColour.slice(1));
+    const { r, g, b } = findComplimentaryColour(colourAsRgb);
+    let complimentaryColour = rgbToHex(r,g,b);
 
     return <div>
-        <span style={{ color: chosenColour, marginLeft: '1rem' }}>chosen: {chosenColour}</span>
-        <span style={{ color: chosenColour, marginLeft: '1rem', filter: 'invert()' }}>complimentary colour: {complimentaryColour}</span>
+        {}
+        <span style={{ color: chosenColour, margin: '1rem' }}>chosen: {chosenColour}</span>
+        <span style={{ color: complimentaryColour, margin: '1rem' }}>complimentary colour: {complimentaryColour}</span>
     </div>
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.padStart(2, '0');
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+/**
+ * 
+ * @param {Object} rgb
+ * @param {Number} rgb.r
+ * @param {Number} rgb.g
+ * @param {Number} rgb.b
+ */
+function findComplimentaryColour({r, g, b}){
+
+    const flip = (v) => (v + 128) % 256
+
+    return {
+        r: flip(r),
+        g: flip(g),
+        b: flip(b)
+    }
+}
+
+
+function hexToRgb(hex) {
+    
+    return {
+        r: parseInt(hex.slice(0,2), 16),
+        g: parseInt(hex.slice(2,4), 16),
+        b: parseInt(hex.slice(4), 16)
+    };
 }
