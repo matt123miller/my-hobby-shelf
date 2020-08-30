548c2762088b991c97c90d625f25d8be12c52dcc
Matt Miller <matt123miller@hotmail.co.uk>
2020-08-30 17:56:17 +0100

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# paint-mixer-app
-
-There's 2 folders.
-
-- 1 for scraping websites to get info about the paints
-- 1 for the front end site that uses that scraped info 
-
-I wanted a little tool for helping me when miniature painting. Something that could suggest complimentary colours
---
+++ precache-manifest.fa41e3a79522425eab1c1df18f3e6fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916cae99a8075e28a060844953178414",
+    "url": "/paint-mixer-app/index.html"
+  },
+  {
+    "revision": "8160cdc2e4b37e11d76e",
+    "url": "/paint-mixer-app/static/css/main.ac90c18b.chunk.css"
+  },
+  {
+    "revision": "ff124f9ea3fdcb3a7e44",
+    "url": "/paint-mixer-app/static/js/2.74e735b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paint-mixer-app/static/js/2.74e735b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8160cdc2e4b37e11d76e",
+    "url": "/paint-mixer-app/static/js/main.69fbad90.chunk.js"
+  },
+  {
+    "revision": "979c78dae70424ef1d10",
+    "url": "/paint-mixer-app/static/js/runtime-main.dc40e223.js"
+  }
+]);
