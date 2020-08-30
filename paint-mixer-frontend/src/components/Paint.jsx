977ae9461afc32fea1c64c613b76e10c76583dc8
Matt Miller <matt123miller@hotmail.co.uk>
2020-08-30 21:45:08 +0100

    Improving layout of the paint blocks

--- paint-mixer-frontend/src/components/Paint.jsx
+++ paint-mixer-frontend/src/components/Paint.jsx
@@ -3,16 +3,24 @@ import React from 'react';
 export default function Paint(props) {
 
     const { paint, onPaintClick } = props;
+    const { hexCode, name } = paint;
 
     return(
-        <li className="paint-container p1" onClick={e => onPaintClick(paint.hexCode)}>
-            <div>
-                Name: {paint.name}
+        <li 
+        className="paint-container"
+        style={{ border: `${hexCode} 5px solid`, borderBottom: `${hexCode} 80px solid` } }
+        onClick={e => onPaintClick(hexCode)}>
+            
+            <div class="paint-name">
+                {name}
             </div>
-            <div>
-                Hex: {paint.hexCode}
+            <div class="paint-code">
+                {hexCode}
             </div>
-            <img className="paint-pot" src={paint.svg} alt={paint.name}/>
+            {/* <div class="paint-pot" style={{ backgroundColor: hexCode }}>
+
+            </div> */}
+            
         </li>
     )
 }
