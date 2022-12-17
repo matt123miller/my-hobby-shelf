f6ca316a4bc10b674bda629c92c7551260b23ee7
Matt Miller <matt@m11r.dev>
2022-12-17 17:51:15 +0000

    Created layout component and using it for main page

--- app/src/components/pages/main.tsx
+++ app/src/components/pages/main.tsx
@@ -1,12 +1,10 @@
 import { LoadingIcon } from "@components/atoms";
 import { PaintCard, PaintList } from "@components/molecules";
-import { SharedProps } from "@typing";
-
-import Head from "next/head";
+import type { SharedProps } from "@typing";
 
 import { trpc } from "../../utils/trpc";
 
-export type MainPageProps = SharedProps<unknown>;
+export type MainPageProps = SharedProps;
 
 export const Main = ({ children, className }: MainPageProps) => {
   const { isLoading, data, error } = trpc.paints.search.useQuery({
@@ -17,30 +15,24 @@ export const Main = ({ children, className }: MainPageProps) => {
 
   if (error) {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <h1>
-          Error occured: <>{error.message}</>
-        </h1>
-      </main>
+      <h1>
+        Error occured: <>{error.message}</>
+      </h1>
     );
   }
 
   if (isLoading) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <LoadingIcon className="h-24 w-24 text-white" />
-      </main>
-    );
+    return <LoadingIcon className="h-24 w-24 text-white" />;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <>
       <p className="m-12 text-white">Main Page</p>
       {data ? (
         <PaintList paints={data.results}></PaintList>
       ) : (
         "Loading tRPC query..."
       )}
-    </main>
+    </>
   );
 };
