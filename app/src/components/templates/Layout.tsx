3a2509a59d45cf0c7595b4454dbe227726324d1c
Matt Miller <matt@m11r.dev>
2023-01-07 16:33:27 +0000

    Fixed nextjs hydration mismatch error
    My layout component included a body tag (because this is what I've done in other frameworks in the past).
    NextJs wraps your whole app in body>div with id __next
    So the DOM is body>div>body which is semantically wrong but more importantly for the error in question you can't nest a body inside a div. The browser console even tells me this.
    Oops.

--- app/src/components/templates/Layout.tsx
+++ app/src/components/templates/Layout.tsx
@@ -11,16 +11,14 @@ export const Layout = ({ children, className }: LayoutProps) => {
         <meta name="description" content="Help choose your paint colours" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
-        <main className="main-content-container flex flex-col">
-          <div
-            className="mx-auto flex min-h-screen w-full flex-col flex-wrap items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] p-5 py-8 sm:py-16"
-            // className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"
-          >
-            {children}
-          </div>
-        </main>
-      </body>
+      <main className="main-content-container flex flex-col">
+        <div
+          className="mx-auto flex min-h-screen w-full flex-col flex-wrap items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] p-5 py-8 sm:py-16"
+          // className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"
+        >
+          {children}
+        </div>
+      </main>
     </>
   );
 };
